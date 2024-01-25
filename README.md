5f99617153a3dd05baceb4ad81f9c06afe2e0122
Arbaaz <arbaaz970@gmail.com>
2024-01-25 12:17:59 +0300

    created project and installed tailwind

---
+++ README.md
@@ -0,0 +1,5 @@
+# Around The World
+
+### View data of countries from around the world. Build using React, TailwindCSS and CountriesAPI.
+
+#### By Arbaaz Sumar
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-red-400">
+        <h1 className="text-6xl text-green-700">Bro</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
